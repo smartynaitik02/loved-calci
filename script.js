2dd21610f5d39ac9b82b640eaed5005172178e41
smartynaitik02 <149299704+smartynaitik02@users.noreply.github.com>
2026-03-11 23:42:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 function calculateLove(){
 
-let name1 = document.getElementById("name1").value;
-let name2 = document.getElementById("name2").value;
+let name1 = document.getElementById("name1").value.toLowerCase().trim();
+let name2 = document.getElementById("name2").value.toLowerCase().trim();
 
 if(name1=="" || name2==""){
     alert("Please enter both names");
     return;
 }
 
-let loveScore = Math.floor(Math.random()*100)+1;
+/* combine both names */
+let combined = name1 + name2;
+
+/* convert names into number */
+let total = 0;
+
+for(let i=0;i<combined.length;i++){
+    total += combined.charCodeAt(i);
+}
+
+/* love percentage */
+let loveScore = (total % 100) + 1;
 
 let resultText = name1 + " ❤️ " + name2 + " = " + loveScore + "%";
 
